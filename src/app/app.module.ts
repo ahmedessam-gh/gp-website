44bef1d02476290ee0b3f9a0f9618b04832a07ad
Ahmed Essam <ae900001@gmail.com>
2022-11-30 15:55:16 +0200

    top nav and header update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { TopHeaderComponent } from './layout/top-header/top-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
