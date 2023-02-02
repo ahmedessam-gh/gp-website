9eb5adfe4b6a2f66f8c2d4682d22ff81d118202e
Ahmed Essam <ae900001@gmail.com>
2023-02-02 02:35:20 +0200

    Revert "Merge branch 'esos-branch' into development"
    This reverts commit 7c4e8e645da2e80f9ac3a5809b443858057da75a, reversing
    changes made to b3d29b0f53f5a3f930e8ae689114f9baefcc174a.

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,7 +1,6 @@
-import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import * as Aos from 'aos';
+import { ProdService } from 'src/app/core/services/prod.service';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +8,13 @@ import * as Aos from 'aos';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+newProds:any
+  constructor( private prod:ProdService) {}
 
   ngOnInit(): void {
 
     this.newProds=this.prod.product
     Aos.init();
-    
   }
 
-
-  
-  
-
-
-  
 }
