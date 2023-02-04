aad3e31e3d0c17643c3d1bdc5705c8ebbd8ee442
Ahmed Essam <ae900001@gmail.com>
2023-02-04 22:32:48 +0200

    .

--- src/app/core/services/prod.service.ts
+++ src/app/core/services/prod.service.ts
@@ -12,56 +12,56 @@ product:Prod[]
       ProdName: "Dress",
       Category: 'Women',
       Price: 100,
-     img: "../../../assets/images/kids.jpeg"
+     img: "assets/images/kids.jpeg"
     },
      {
       id: 2,
       ProdName: "jeans",
       Category: 'Women',
       Price: 100,
-     img: "../../../assets/images/kids.jpeg"
+     img: "assets/images/kids.jpeg"
     },
    {
       id: 3,
       ProdName: "Dress",
       Category: 'Women',
       Price: 100,
-     img: "../../../assets/images/kids.jpeg"
+     img: "assets/images/kids.jpeg"
     },
    {
       id: 4,
       ProdName: "Dress Kids",
       Category: 'Women',
       Price: 100,
-     img: "../../../assets/images/kids.jpeg"
+     img: "assets/images/kids.jpeg"
     },
      {
       id: 5,
       ProdName: "Dress",
       Category: 'Women',
       Price: 100,
-     img: "../../../assets/images/dressify-logo-zip-file (1)/png/logo-black.png"
+     img: "assets/images/dressify-logo-zip-file (1)/png/logo-black.png"
     },
     {
       id: 4,
       ProdName: "Casual Wear",
       Price: 100,
       Category: 'Women',
-     img: "../../../assets/images/casual.jpg"
+     img: "assets/images/casual.jpg"
     },
     {
       id: 4,
       ProdName: "Kids Wear",
       Price: 100,
       Category: 'Women',
-     img: "../../../assets/images/kids.jpeg"
+     img: "assets/images/kids.jpeg"
     },
     {
       id: 4,
       ProdName: "accessory",
       Category: 'Women',
       Price: 100,
-     img: "../../../assets/images/accessory.jpg"
+     img: "assets/images/accessory.jpg"
     },
 
     ]
