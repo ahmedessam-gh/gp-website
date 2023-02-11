88fc17cc06c2727bf1257a68c9c06ca14ba735c7
Ahmed Essam <ae900001@gmail.com>
2023-02-11 10:33:20 +0200

    eventoncart

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -9,10 +9,28 @@ import { ProdService } from 'src/app/core/services/prod.service';
 })
 export class HomeComponent implements OnInit {
   newProds: any;
-  constructor(private prod: ProdService) {}
-
+  constructor(private prod: ProdService) { }
+  imgUrl: string = '';
+  category: string = '';
+  gender: string = '';
+  price: string = '';
+  favouritItem: object = {};
   ngOnInit(): void {
     this.newProds = this.prod.product;
     Aos.init({});
+  }
+
+  function($event) {
+    this.imgUrl = $event.target.parentNode.parentNode.childNodes[0].src;
+    this.category = $event.target.parentNode.parentNode.parentNode.childNodes[1].innerText;
+    this.gender = $event.target.parentNode.parentNode.parentNode.childNodes[2].innerText;
+    this.price = $event.target.parentNode.parentNode.parentNode.childNodes[3].innerText;
+    this.favouritItem = {
+      url: this.imgUrl,
+      category: this.category,
+      gender: this.gender,
+      price: this.price
+    }
+    console.log(this.favouritItem);
   }
 }
