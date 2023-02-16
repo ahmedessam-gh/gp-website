decfd1faf7ffe56c7a8be7153abadf351dbc6277
Ahmed Essam <ae900001@gmail.com>
2023-02-16 14:37:51 +0200

    ,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -36,7 +36,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 
   ],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/views/cart/cart.module.ts
+++ src/app/views/cart/cart.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './cart.component';
+import { MainButtonModule } from "../../shared/main-button/main-button.module";
 
-
+import {
+    FontAwesomeModule,
+    FaIconLibrary,
+  } from '@fortawesome/angular-fontawesome';
 @NgModule({
-  declarations: [
-    CartComponent
-  ],
-  imports: [
-    CommonModule,
-    CartRoutingModule,
-    FormsModule
-  ]
+    declarations: [
+        CartComponent
+    ],
+    imports: [
+        CommonModule,
+        CartRoutingModule,
+        FormsModule,
+        MainButtonModule,
+        FontAwesomeModule
+    ]
 })
 export class CartModule { }
