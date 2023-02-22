6e05e7042831fa4f8df919a36f393f9e158e2940
Ahmed Essam <ae900001@gmail.com>
2023-02-22 09:22:09 +0200

    user

--- src/app/views/profile/profile.module.ts
+++ src/app/views/profile/profile.module.ts
@@ -5,17 +5,22 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-
-
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { MainButtonModule } from "../../shared/main-button/main-button.module";
 @NgModule({
-  declarations: [
-    ProfileComponent,
-    SidebarComponent,
-    ProfileDetailsComponent
-  ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+    declarations: [
+        ProfileComponent,
+        SidebarComponent,
+        ProfileDetailsComponent
+    ],
+    imports: [
+        CommonModule,
+        ProfileRoutingModule,
+        FontAwesomeModule,
+        MainButtonModule
+    ]
 })
 export class ProfileModule { }
