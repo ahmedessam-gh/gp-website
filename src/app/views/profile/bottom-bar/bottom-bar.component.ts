bd654bca7e0ee5ebe2b00cc4b89932e2a76c0157
Ahmed Essam <ae900001@gmail.com>
2023-02-28 19:38:24 +0200

    sidebar close and open (responsive)

--- src/app/views/profile/bottom-bar/bottom-bar.component.ts
+++ src/app/views/profile/bottom-bar/bottom-bar.component.ts
@@ -10,21 +10,32 @@ export class BottomBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const leftBtn =document.getElementById('leftBtn');
+    const navRespons = document.getElementById('navRespons');
+    document.addEventListener('click',function(event){
+      if((event.target as HTMLElement).id !== 'navRespons' && (event.target as HTMLElement).id !== 'leftBtn'){
+        navRespons.classList.remove('d-block');
+        navRespons.classList.add('d-none');
+        document.body.children[0].classList.remove('layout');
+        leftBtn.classList.remove('d-none');
+      }
+    });
   }
+
+  
   showRightBar(){
     const leftBtn =document.getElementById('leftBtn');
     const navRespons = document.getElementById('navRespons');
-    navRespons.classList.add('d-block');
-    navRespons.classList.remove('d-none');
-    leftBtn.classList.add('d-none');
-    document.body.classList.add('');
+    navRespons.classList.toggle('d-none');
+    leftBtn.classList.toggle('d-none');
+    document.body.children[0].classList.add('layout');
   }
   closeRightBar(){
     const leftBtn =document.getElementById('leftBtn');
     const navRespons = document.getElementById('navRespons');
-    leftBtn.classList.add('d-block');
-    leftBtn.classList.remove('d-none');
-    navRespons.classList.add('d-none');
-    
+    leftBtn.classList.toggle('d-none');
+    navRespons.classList.toggle('d-none');
+    document.body.children[0].classList.remove('layout');
   }
+  
 }
