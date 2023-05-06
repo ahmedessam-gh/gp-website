9960c16154a63dc737188153a99db6befa9d141d
Ahmed Essam <ae900001@gmail.com>
2023-05-06 23:58:04 +0200

    loginvalidation

--- src/app/views/welcome/loginn/loginn.component.ts
+++ src/app/views/welcome/loginn/loginn.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, Validators ,FormGroup} from '@angular/forms';
 import { Router } from '@angular/router';
 import { error } from 'console';
@@ -14,8 +14,8 @@ export class LoginnComponent implements OnInit {
   loginForm:FormGroup;
   submitted:boolean = false;
   loading = false;
-  errors:string;
-  errors1:string;
+  errors:string = '';
+  
   constructor(private fb : FormBuilder,private auth:AuthService,private router:Router) { }
 
   ngOnInit(): void {
@@ -27,21 +27,22 @@ export class LoginnComponent implements OnInit {
   }
   login(){
     this.submitted = true;
-    // this.loading = true;
+    this.loading = true;
     if(this.loginForm.valid){
+      
       this.auth.login(this.loginForm.value).subscribe(next => {
-        // this.loading = false;
+        this.loading = false;
         this.router.navigate(['/shop']);
         
       },error => {
-        // this.loading = false;
+        this.loading = false;
         console.log(error.error);
-        this.errors1 = error.error;
-      
+        this.errors = error.error;
       })
     }else{
       this.loading = false;
       this.errors = "Please Fill Your Email or Password";
     }
+   
   }
 }
