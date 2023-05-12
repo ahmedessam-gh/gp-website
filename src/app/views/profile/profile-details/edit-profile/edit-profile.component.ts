b33909aa2d18fd6ba56149cebd50f5688042cfbd
Ahmed Essam <ae900001@gmail.com>
2023-05-12 02:54:22 +0200

    fixes

--- src/app/views/profile/profile-details/edit-profile/edit-profile.component.ts
+++ src/app/views/profile/profile-details/edit-profile/edit-profile.component.ts
@@ -20,37 +20,36 @@ export class EditProfileComponent implements OnInit {
       this.userDetails = data;
       console.log(this.userDetails);
       this.userInfo = this.fb.group({
-        fName: [this.userDetails.fName],
-        lName: [this.userDetails.lName],
-        email: [this.userDetails.email,Validators.email],//design error
-        address: [this.userDetails.address],
-        phoneNumber: [this.userDetails.phoneNumber]
+        fName: [this.userDetails.fName || ''],
+        lName: [this.userDetails.lName || ''],
+        email: [this.userDetails.email || '',Validators.email],//design error
+        address: [this.userDetails.address || ''],
+        phoneNumber: [this.userDetails.phoneNumber || '']
       });
       this.userImg = this.fb.group({
         imgUrl:[this.userDetails.imgUrl],
-      })
+      });
+      console.log(this.userInfo.value);
     });
     
   }
  
   changeDetails(){
+    console.log(this.userInfo);
     if (this.userInfo.valid) {
-      this.profile.editDetails(this.userInfo.value).subscribe(next => {
+      this.profile.editDetails(this.userInfo.value).subscribe(() => {
+        // console.log(this.userInfo);
         alert('user details updated');
-        this.profile.getDetails().subscribe((data)=>{
-          this.userDetails = data;
-          console.log(this.userDetails);
-          this.router.navigate(['profile/details']);
-        })
+        this.router.navigate(['profile/details']);
       });
     }
   }
   changeImage(e){
-    console.log(e.target.files[0]);
+    // console.log(e.target.files[0]);
     // this.userImg.get('imgUrl') = e.target.files[0].ur
     const formData = new FormData();
     formData.append('photo',e.target.files[0], e.target.files[0].name);
-    console.log(formData);
+    // console.log(formData);
     this.auth.changeImg(formData).subscribe(data => {     
       this.userImg.get('imgUrl').patchValue(data['url']);   
     },error=>{
