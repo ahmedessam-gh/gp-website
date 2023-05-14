2cc94274ed98b4758ff807e38bbb29047b01b20e
RamHussein <ram_hussein@live.com>
2023-05-14 19:57:10 +0300

    essam commits

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,20 +1,43 @@
 import { Injectable } from '@angular/core';
 import { cart } from '../interfaces/cart';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { apiEndpoints } from '../api-endpoints';
-import { map } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { orderDetails } from '../interfaces/orderDetails';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  constructor(private http:HttpClient) {}
-  viewCart(){
-    return this.http.get(`${apiEndpoints.baseUrl}${apiEndpoints.carts.getCustomerCart}`);
+  order: any;
+  orderTotal: number = 0
+  constructor(private http: HttpClient) { }
+  viewCart(pNumber: number, pSize: number): Observable<any[]> {
+    return this.http.get<any[]>(`${apiEndpoints.baseUrl}${apiEndpoints.carts.getCustomerCart(pNumber, pSize)}`);
   }
-  
-  deleteCart(id){
+  deleteCart(id) {
     return this.http.delete(`${apiEndpoints.baseUrl}${apiEndpoints.carts.removeFromCart(id)}`);
   }
-  
+  incrementQuantity(param: HttpParams) {
+    return this.http.put(`${apiEndpoints.baseUrl}${apiEndpoints.carts.incrementQuantity}`, '', { params: param });
+  }
+  decrementQuantity(param: HttpParams) {
+    return this.http.put(`${apiEndpoints.baseUrl}${apiEndpoints.carts.decrementQuantity}`, '', { params: param });
+  }
+  getOrderDetails(): Observable<orderDetails> {
+    return this.http.get<orderDetails>(`${apiEndpoints.baseUrl}${apiEndpoints.carts.getOrderSummary}`);
+  }
+
+  async totalPrice(): Promise<number> {
+    this.orderTotal = 0;
+    this.order = await this.getOrderDetails().toPromise();
+    this.order.detailsList.forEach((prod) => {
+      this.orderTotal += prod.price;
+    });
+    return this.orderTotal;
+  }
+
+  payWithCash(payUpdates:any){
+    return this.http.post(`${apiEndpoints.baseUrl}${apiEndpoints.carts.payWithCash}`,payUpdates);
+  }
 }
