c07c86c2d6d1d35aa545525661a190cd76a857d8
Ahmed Essam <ae900001@gmail.com>
2023-05-21 05:20:49 +0300

    report product

--- src/app/views/report/report.component.ts
+++ src/app/views/report/report.component.ts
@@ -1,8 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm, Validators } from '@angular/forms';
-import { FormBuilder,FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import * as Aos from 'aos';
+import { delay } from 'rxjs';
 import { CustomerService } from 'src/app/core/services/customer.service';
+import { NgbService } from 'src/app/core/services/ngb.service';
 
 @Component({
   selector: 'app-report',
@@ -12,30 +15,35 @@ import { CustomerService } from 'src/app/core/services/customer.service';
 
 export class ReportComponent implements OnInit {
   submitted = false;
-  reportForm:FormGroup;
-  reportData:any;
-  constructor(private fb:FormBuilder,private customer:CustomerService){}
- 
+  reportForm: FormGroup;
+  reportData: any;
+  toastrMsg: any;
+  constructor(private fb: FormBuilder, private customer: CustomerService, private ngb: NgbService, private router: Router) { }
+
   ngOnInit(): void {
     Aos.init({});
     this.reportData = this.customer.getReportData();
-    console.log(this.reportData.productName);
+    console.log(this.reportData);
     this.reportForm = this.fb.group({
-      productId:[{value:this.reportData.productId,disabled:true}],
-      description:['',Validators.required],
-      date:new Date().toISOString()
+      // productId:[{value:this.reportData.productId,disabled:true}],
+      productId: ['' || this.reportData.productId],
+      description: ['', Validators.required],
     })
-    
+
   }
 
-  
-  reportProduct() {
+
+  reportProduct(msg: any) {
     this.submitted = true;
-    if(this.reportForm.valid){
-      this.customer.reportProduct(this.reportForm.value).subscribe(()=>{
-        console.log('Product has been reported successfuly!');
-      })
+    this.toastrMsg = msg;
+    if (this.reportForm.valid) {
+      this.customer.reportProduct(this.reportForm.value).subscribe();
+      this.ngb.show(this.toastrMsg);
+      setTimeout(() => {
+        this.router.navigate(['/shop']);
+      }, 3000)
     }
+
   }
 
 }
